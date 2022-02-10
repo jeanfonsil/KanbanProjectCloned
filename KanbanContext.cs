297c1c352c6674333aaf6003aaa8e830185f2793
Jean Fonseca da Silva <jeanfonseca15@gmail.com>
2022-02-09 23:35:00 -0300

    Context Create

---
+++ KanbanContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanProject
+{
+    public class KanbanContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Sprint> Sprints { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=KanbanDb;Trusted_Connection=true;");
+        }
+    }
+}
